f8d20e80a60fc91b72c547b60095dfe691700d32
DoseiBrayin <ardilabrayan68@gmail.com>
2022-11-18 20:11:01 -0500

    (add) Add feature post insert in database

--- src/routes/links.js
+++ src/routes/links.js
@@ -7,11 +7,11 @@ router.get('/add', (req, res) => {
     res.render('links/add')
 })
 
-router.post('/add', (req, res) => {
+router.post('/add', async(req, res) => {
     let color = ''
     if(req.body.rol=="Coordinador"){
         color='red'
-    }else if(req.req.body.rol=="Jefe de area"){
+    }else if(req.body.rol=="Jefe de area"){
         color='blue'
     }
     else if(req.body.rol=="Impulsador"){
@@ -21,14 +21,14 @@ router.post('/add', (req, res) => {
     }
     const empleado = {
         "nombre": req.body.nombre,
+        "rol": req.body.rol,
         "correo": req.body.correo,
         "cedula": req.body.cedula,
         "dir": req.body.dir,
-        "rol": req.body.rol,
-        "color":color
+        "celular":req.body.cel
     }
-    
-    res.send('recivido')
+    await pool.query('INSERT INTO empleado set ?', [empleado]);
+    res.render('links/add')
 })
 
 module.exports = router
