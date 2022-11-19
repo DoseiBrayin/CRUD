d3699b4aa21ef55c4e40d6dfd5276fd5a7ad242d
DoseiBrayin <ardilabrayan68@gmail.com>
2022-11-18 19:51:49 -0500

    (add) navegation

--- src/routes/links.js
+++ src/routes/links.js
@@ -3,5 +3,32 @@ const express = require('express')
 const router = express.Router()
 const pool = require('../database')
 
+router.get('/add', (req, res) => {
+    res.render('links/add')
+})
+
+router.post('/add', (req, res) => {
+    let color = ''
+    if(req.body.rol=="Coordinador"){
+        color='red'
+    }else if(req.req.body.rol=="Jefe de area"){
+        color='blue'
+    }
+    else if(req.body.rol=="Impulsador"){
+        color='violet'
+    }else{
+        color='green'
+    }
+    const empleado = {
+        "nombre": req.body.nombre,
+        "correo": req.body.correo,
+        "cedula": req.body.cedula,
+        "dir": req.body.dir,
+        "rol": req.body.rol,
+        "color":color
+    }
+    
+    res.send('recivido')
+})
 
 module.exports = router
